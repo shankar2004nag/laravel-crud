b67a8f6c024db61888ed68cc847121a9fbba57b2
Shanky <insoftservice@yahoo.com>
2018-10-27 00:45:59 +0530

    DB

--- README.md
+++ README.md
@@ -2,3 +2,7 @@ LARAVEL AJAX CRUD
 
 Just small code for begineers 
 Have focused only on functionality currently but soon would try to improve design.
+
+To check crud 
+Domain.com/blog or localhost/blog 
+
---
+++ sql.sql
@@ -0,0 +1,38 @@
+/*
+SQLyog Professional v12.4.3 (64 bit)
+MySQL - 10.1.32-MariaDB : Database - laravel_ajax_crud
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`laravel_ajax_crud` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `laravel_ajax_crud`;
+
+/*Table structure for table `blog_post` */
+
+DROP TABLE IF EXISTS `blog_post`;
+
+CREATE TABLE `blog_post` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) DEFAULT NULL,
+  `description` text,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  `deleted_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `blog_post` */
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
